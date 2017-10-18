e9aed7ba196541519d72fb121966b498abd6a7eb
pc-04-eclips2 <pc-04@pc-04-PC-eclips2>
2017-10-18 14:28:15 +0900

    test

--- 1018/src/git.java
+++ 1018/src/git.java
@@ -1,8 +1,13 @@
 public class git {
-public static void min(String[]arg){
-	System.out.println("hi");
-	System.out.println("hello");
-	System.out.println("How R U?");
-	System.out.println("im fine thanks");
-	System.out.println("haha");}
+	public static void min(String[] arg) {
+		System.out.println("hi");
+		System.out.println("hello");
+		System.out.println("How R U?");
+		System.out.println("im fine thanks");
+		System.out.println("haha");
+	}
+
+	public git() {
+		System.out.println("b");
+	}
 }
