6a1d8aa96abbdfcd3ac94b7e56873b2cbe4b821f
pc-04-eclips2 <pc-04@pc-04-PC-eclips2>
2017-10-18 14:20:02 +0900

    show

--- 1018/src/git.java
+++ 1018/src/git.java
@@ -3,5 +3,6 @@ public static void min(String[]arg){
 	System.out.println("hi");
 	System.out.println("hello");
 	System.out.println("How R U?");
-}
+	System.out.println("im fine thanks");
+	System.out.println("haha");}
 }
